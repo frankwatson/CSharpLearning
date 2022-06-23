1d481507ab832b869402ae0122eb902dd332c316
Frank Watson <frank@watsons.io>
2022-06-23 16:45:05 -0400

    Section 5

---
+++ Intermediate Course/AccessModifiers2/AccessModifiers2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+
+namespace AccessModifiers2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+        }
+    }
+}
---
+++ Intermediate Course/UpcastingAndDowncasting/UpcastingAndDowncasting/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpcastingAndDowncasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // UPCASTING
+
+            // Text text = new Text();
+            //
+            // // Upcasting
+            // // Implicitly convert an object reference (text) to a space class reference (shape)
+            // Shape shape = text;
+            //
+            // // Points to the same object in memory so it is updated
+            // text.Width = 200;
+            // shape.Width = 100;
+            //
+            // Console.WriteLine(text.Width);
+
+            // StreamReader reader = new StreamReader(new MemoryStream());
+
+            // // Can store any type w/o conversion
+            // var list = new ArrayList();
+            // list.Add(1);
+            // list.Add("Frank");
+            // list.Add(new Text());
+            //
+            // // Restricted list by one type
+            // var anotherList = new List<Shape>();
+
+            // DOWNCASTING
+
+            Shape shape = new Text();
+            Text text = (Text)shape;
+            
+        }
+    }
+}
