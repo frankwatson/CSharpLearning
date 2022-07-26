ac60d6ccc74b3ffd70ecff088166d5e84179f0ad
Frank Watson <frank@watsons.io>
2022-07-26 19:59:07 -0400

    Completion of Section 7

---
+++ Entity Framework/Section7Excercises/Vidzy/Vidzy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity;
+
+namespace Vidzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new VidzyContext();
+
+            // Lazy Loading
+            Console.WriteLine("+-+-+-+-+-+-+-+-+");
+            var videos = context.Videos.ToList();
+
+            foreach (var v in videos)
+                Console.WriteLine("{0} ({1})", v.Name, v.Genre.Name);
+
+            Console.WriteLine("+-+-+-+-+-+-+-+-+");
+
+            // Eager Loading
+            var videosWithGenres = context.Videos.Include(v => v.Genre).ToList();
+
+            foreach (var v in videosWithGenres)
+                Console.WriteLine("{0} ({1})", v.Name, v.Genre.Name);
+            
+            Console.WriteLine("+-+-+-+-+-+-+-+-+");
+
+            // Explicit Loading
+            context.Genres.Load();
+
+            foreach(var v in videos)
+                Console.WriteLine("{0} ({1})", v.Name, v.Genre.Name);
+            
+            Console.WriteLine("+-+-+-+-+-+-+-+-+");
+        }
+    }
+}
